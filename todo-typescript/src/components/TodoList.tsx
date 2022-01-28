1241ae5577cf4b82d6e0ed6b83f2d56d5c1a5d4b
Nick <nsagias@gmail.com>
2022-01-28 00:29:32 -0500

    loop through items array add short circuit and add if array is empty

--- todo-typescript/src/components/TodoList.tsx
+++ todo-typescript/src/components/TodoList.tsx
@@ -1,10 +1,9 @@
-const TodoList: React.FC = () => {
+const TodoList: React.FC<{items: string[]}> = (props) => {
   return (
     <div>
       <ul>
-        <li>todo</li>
-        <li>todo</li>
-        <li>todo</li>
+        {Array.isArray(props.items) && props.items.map((item, index) => <li key={index}>{item}</li>)}
+        {!Array.isArray(props.items) && <div>empty todo list</div>}
       </ul>
     </div>
   );
