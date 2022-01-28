d68ac076c955a387ebbb79875750cd69d3eca86d
Nick <nsagias@gmail.com>
2022-01-28 00:35:21 -0500

    create prop on compoent list

--- todo-typescript/src/App.tsx
+++ todo-typescript/src/App.tsx
@@ -4,7 +4,7 @@ import TodoList from './components/TodoList';
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <TodoList items={["get this done", "get it done yesterday"]}/>
     </div>
   );
 }
