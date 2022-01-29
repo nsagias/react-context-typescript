fd2b25ef7dc5eba62b211277bfe55d710ea6d432
Nick <nsagias@gmail.com>
2022-01-28 23:19:22 -0500

    create interface, eventhandler, and add eventhandler to form

--- todo-typescript/src/components/NewTodo.tsx
+++ todo-typescript/src/components/NewTodo.tsx
@@ -1,6 +1,17 @@
+interface IhandleSubmit {
+  (event: any): void;
+} 
+
 const NewTodo: React.FC = (props) => {
+
+  const handleSubmit: IhandleSubmit = (event) => {
+    event.preventDefault();
+  };
+  
   return  (
-    <></>
+    <form onSubmit={handleSubmit}>
+
+    </form>
   );
 };
 export default NewTodo;
