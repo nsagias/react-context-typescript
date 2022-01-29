3fd73194fa871ffcdc0948c2daa64bdd244d9c81
Nick <nsagias@gmail.com>
2022-01-28 22:41:21 -0500

    clean code

--- todo-typescript/src/components/TodoList.tsx
+++ todo-typescript/src/components/TodoList.tsx
@@ -7,10 +7,6 @@ const TodoList: React.FC<{items: Todo[]}> = (props) => {
 
   return (
     <>
-      {/* <ul>
-        {Array.isArray(props.items) && props.items.map(item => <li key={item.id}>{item.text}</li>)}
-        {!Array.isArray(props.items) && <div>empty todo list</div>}
-      </ul> */}
       <TodoItem items={items}/>
     </>
   );
