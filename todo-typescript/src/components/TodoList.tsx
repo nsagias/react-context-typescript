9bf9c43b7151b34a1652ab132f2b2f81feb6d8eb
Nick <nsagias@gmail.com>
2022-01-28 22:57:26 -0500

    Add unorderlist listtag and format looping through array

--- todo-typescript/src/components/TodoList.tsx
+++ todo-typescript/src/components/TodoList.tsx
@@ -6,14 +6,14 @@ const TodoList: React.FC<{items: Todo[]}> = (props) => {
   const {items} = props;
 
   return (
-    <>
-     
+    <ul>
       {/* {Array.isArray(props.items) && props.items.map(item => <li key={item.id}>{item.text}</li>)} */}
-      {Array.isArray(props.items) 
-          && props.items.map(item => <TodoItem key={item.id} text={item.text} />)}
-
-      {!Array.isArray(props.items) && <div>empty todo list</div>}
-    </>
+      {Array.isArray(props.items) && props.items.map(item => 
+        <TodoItem key={item.id} text={item.text} />
+      )}
+        
+      {!Array.isArray(props.items) && <div>empty todo list</div>}  
+    </ul>
   );
 }
 export default TodoList;
