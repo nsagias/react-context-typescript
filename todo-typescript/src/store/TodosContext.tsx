c4c63cf9720da0bfa718e8b6b5100c30900d0847
Nick <nsagias@gmail.com>
2022-01-29 19:45:19 -0500

    Create todocontext

--- todo-typescript/src/store/TodosContext.tsx
+++ todo-typescript/src/store/TodosContext.tsx
@@ -1,7 +1,12 @@
-const TodosContext = () => {
-  return (
-    <></>
-  );
-};
+import React, {createContext} from "react";
+import Todo from "../models/todo";
+
+
+
+const TodosContext = createContext({
+  items: [],
+  addTodo: () => {},
+  deleteTodo: (id: string) => {}
+});
 
 export default TodosContext;
