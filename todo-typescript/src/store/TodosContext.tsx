716522c047205ce60f2f949a420de6f5248b01d3
Nick <nsagias@gmail.com>
2022-01-29 19:45:55 -0500

    Create interface for todocontext

--- todo-typescript/src/store/TodosContext.tsx
+++ todo-typescript/src/store/TodosContext.tsx
@@ -1,9 +1,13 @@
 import React, {createContext} from "react";
 import Todo from "../models/todo";
 
+interface ITodoContext {
+  items: Todo[];
+  addTodo: () => void;
+  deleteTodo: (id: string) => void; 
+}
 
-
-const TodosContext = createContext({
+const TodosContext = createContext<ITodoContext>({
   items: [],
   addTodo: () => {},
   deleteTodo: (id: string) => {}
