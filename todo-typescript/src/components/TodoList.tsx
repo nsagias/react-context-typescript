65d8702120c4ee36612394fedd2c453d7792d6a3
Nick <nsagias@gmail.com>
2022-01-29 20:15:01 -0500

    remove types for props as it uses context now, update all props to use todoCtx, update function names to onDeleteTodo to deleteTodo

--- todo-typescript/src/components/TodoList.tsx
+++ todo-typescript/src/components/TodoList.tsx
@@ -5,22 +5,21 @@ import { TodosContext } from "../store/TodosContext";
 import classes from "./TodoList.module.css";
 import { useContext } from "react";
 
-const TodoList: React.FC<{items: Todo[]; onDeleteTodo: (id: string) => void}> = (props) => {
+const TodoList: React.FC = () => {
   const todosCtx = useContext(TodosContext);
-
 
   return (
     <ul className={classes.todos}>
       {/* {Array.isArray(props.items) && props.items.map(item => <li key={item.id}>{item.text}</li>)} */}
-      {Array.isArray(props.items) && props.items.map(item => 
+      {Array.isArray(todosCtx.items) && todosCtx.items.map(item => 
         <TodoItem 
           key={item.id} 
           text={item.text} 
-          onDeleteTodo={props.onDeleteTodo.bind(null, item.id)}  
+          onDeleteTodo={todosCtx.deleteTodo.bind(null, item.id)}  
         /> 
       )}
         
-      {!Array.isArray(props.items) && <div>empty todo list</div>}  
+      {!Array.isArray(todosCtx.items) && <div>empty todo list</div>}  
     </ul>
   );
 }
